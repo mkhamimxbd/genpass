b450c3d3d458ba52954bfad5da7dbb1f472eccc0
M. K. Hamim <mkhamimbd@gmail.com>
2026-02-12 11:54:00 +0600

    Alpha 0.2.0. Added toggle options and length for password generation

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,48 @@
-const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const passwordLengthInput = document.querySelector('.js-password-length-input');
+const generatePasswordBtn = document.querySelector('.js-generate-password-btn');
+const passwordDiv = document.querySelector('.js-password-div');
 
-document.querySelector('.btn').addEventListener('click', () => {
-  console.log(generatePassword());
-})
+const includeUppercase = document.querySelector('.js-include-uppercase');
+const includeLowercase = document.querySelector('.js-include-lowercase');
+const includeNumbers = document.querySelector('.js-include-numbers');
+const includeSymbols = document.querySelector('.js-include-symbols');
 
-function generatePassword() {
+const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+const numberChars = '1234567890';
+const symbolChars = `'~!@#$%^&*(){}[]_-+=/<>,.;:'`;
+
+
+
+generatePasswordBtn.addEventListener('click', () => {
+  passwordDiv.textContent = `Password: ${generatePassword()}`;
+});
+
+function generatePassword(uppercase, lowercase, numbers, symbols) {
+  uppercase = includeUppercase.checked;
+  lowercase = includeLowercase.checked;
+  numbers = includeNumbers.checked;
+  symbols = includeSymbols.checked;
+
+  let allowedChars = '';
+
+  allowedChars += uppercase ? uppercaseChars : '';
+  allowedChars += lowercase ? lowercaseChars : '';
+  allowedChars += numbers ? numberChars : '';
+  allowedChars += symbols ? symbolChars : '';
+
+  const length = Number(passwordLengthInput.value);
   let password = '';
-  for (let i = 0; i < 12; i ++) {
-    const randomNum = Math.floor(Math.random() * 25) + 1;
-    password += uppercase.charAt(randomNum);
+
+  if (length <= 0) {
+    alert('Length cannot be less than 1.')
+  } else if (allowedChars.length === 0) {
+    alert('Select at least one Charset.')
+  }
+
+  for (let i = 0; i < length; i ++) {
+    const randomNum = Math.floor(Math.random() * allowedChars.length) + 1;
+    password += allowedChars.charAt(randomNum);
   }
 
   return password;
